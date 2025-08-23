955b2c838bdf2ac3d54615314d3babad2a957542
1Yevhen1 <genyafedoriv@gmail.com>
2025-08-23 23:15:38 +0300

    goit-js-hw-11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "51935119-94cca84236608ef27f9ff626b"; 
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
