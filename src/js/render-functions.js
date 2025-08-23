1508c3cbc1b825b11fa0a1dba92e2aba6d571162
1Yevhen1 <genyafedoriv@gmail.com>
2025-08-23 23:40:18 +0300

    goit-js-hw-11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,7 +4,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const gallery = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
 
-// екземпляр SimpleLightbox
+
 const lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
